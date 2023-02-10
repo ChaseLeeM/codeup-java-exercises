642c8c0af47978442beb63ec2cda37d021b53513
Chase Medford <chasemedford1@gmail.com>
2023-02-10 16:26:52 -0600

    ControlFlow Exercise Started

---
+++ src/StringExercise.java
@@ -0,0 +1,26 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String brick = """
+                We don't need no education
+                We don't need no thought control
+                """;
+        System.out.println(brick);
+        String check = """
+                Check "this" out!, "s inside of "s!
+                """;
+        System.out.println(check);
+        String window = """
+                In windows, the main drive is usually C:\\
+                """;
+        System.out.println(window);
+        String backslash = """
+                I can do backslashes \\, double backslashes \\\\,
+              and the amazing triple backslash \\\\\\!""";
+        System.out.println(backslash);
+
+
+
+
+
+    }
+}
